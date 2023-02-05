5c39b3162e42a11ffcc54dd0b435112cce0fdfe8
paulhorda <paulhorda02480@gmail.com>
2023-02-05 21:31:10 +0200

    Create AndroidApp for Hilt

---
+++ app/src/main/java/com/example/repaircalculator/RepairCalculator.java
@@ -0,0 +1,8 @@
+package com.example.repaircalculator;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class RepairCalculator extends Application{}
